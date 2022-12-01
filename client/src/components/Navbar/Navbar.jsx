c9c84886b937b2eee0dc6411b1057e9733588111
sunjuyoung <syseoz@naver.com>
2022-12-01 19:25:01 +0900

    add footer nav home-slider

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import {
+Link
+  } from "react-router-dom";
+import './Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='wrapper'>
+            <div className='left'>
+
+                <div className="item">
+                    <img src='/img/ko.png' alt='' />
+                    <KeyboardArrowDownIcon />
+                </div>
+
+                <div className='item'>
+                    <span>Ko</span>
+                    <KeyboardArrowDownIcon />
+                </div>
+
+                <div className='item'>
+                    <Link className='link' to="/products/1">Women</Link>
+                </div>
+                <div className='item'>
+                    <Link className='link' to="/products/2">Men</Link>
+                </div>
+                <div className='item'>
+                    <Link className='link' to="/products/3">Children</Link>
+                </div>
+
+            </div>
+            {/* left end */}
+            <div className='center'>
+                <Link to="/">JAVA STORE</Link>
+            </div>
+
+            <div className='right'>
+
+            <div className='item'>
+                <Link className='link' to="/">Hompage</Link>
+            </div>
+            <div className='item'>
+                <Link className='link' to="/">About</Link>
+            </div>
+            <div className='item'>
+                <Link className='link' to="/">Contact</Link>
+            </div>
+            <div className='item'>
+                <Link className='link' to="/">Stores</Link>
+            </div>
+            <div className='icons'>
+                <SearchIcon />
+                <PersonOutlineIcon />
+                <FavoriteBorderIcon />
+                <div className='cartIcon'>
+                    <ShoppingCartOutlinedIcon />
+                    <span>0</span>
+                </div>
+            </div>
+
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ client/src/pages/Products/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Products.scss';
+
+const Products = () => {
+  return (
+    <div className='products'>
+
+    </div>
+  )
+}
+
+export default Products
